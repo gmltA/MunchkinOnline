1c53ecf7bacd6a77a41f2c358db6b90864331128
gmlt.A <gmltt@yandex.ru>
2014-09-13 07:17:12 +0300

    Notification manager

--- Munchkin Online/Content/js/Core.js
+++ Munchkin Online/Content/js/Core.js
@@ -1,38 +1,66 @@
-﻿function showNotification(message) {
+﻿var notificationQueue = [];
+var isNotificationActive = false;
+
+function showNotification(message)
+{
+    isNotificationActive = true;
     $(".notification").addClass("anim");
-    $(".notification").animate({ bottom: '0' }, 250, "swing", function () {
-        $(".notification > .center").animate({ width: '190px' }, 500, "swing", function () {
+    $(".notification").animate({bottom : '0'}, 250, "swing", function(){
+        $(".notification > .center").animate({width : '190px'}, 500, "swing", function(){
             $(".notification > .scroll").hide();
             $(".notification").toggleClass("anim");
             $(".notification > P").text(message);
         })
     });
-    setTimeout(function () {
+    setTimeout(function()
+    {
         $(".notification").toggleClass("anim");
         $(".notification > P").text("");
         $(".notification > .scroll").show();
-        $(".notification > .center").animate({ width: '0px' }, 500, "swing", function () {
-            $(".notification").animate({ bottom: '-200px' }, 250, "swing")
+        $(".notification > .center").animate({width : '0px'}, 500, "swing", function(){
+            $(".notification").animate({bottom : '-200px'}, 250, "swing", function(){
+                isNotificationActive = false;
+                displayNextNotification();
+            })
         });
-    }, 1000);
+	}, 1000);
 }
 
-function toggleFriendsBar(controlButton) {
-    if ($(controlButton).hasClass("down")) {
-        $(controlButton).parent().find(".container").slideDown();
-        $(controlButton).animate({ marginTop: "-19px" }, "fast");
-    }
-    else {
-        $(controlButton).parent().find(".container").slideUp();
-        $(controlButton).animate({ marginTop: "0px" }, "fast");
-    }
-    $(controlButton).toggleClass("down");
+function displayNextNotification()
+{
+    if (notificationQueue.length == 0)
+        return;
+
+    showNotification(notificationQueue.pop());
 }
 
-$(document).ready(function () {
-    showNotification("Kyky!");
+function addNotification(message)
+{
+    notificationQueue.push(message);
+    if (isNotificationActive == false)
+        displayNextNotification();
+}
 
-    $("HEADER .control").live("click", function () {
+function toggleFriendsBar(controlButton)
+{
+    if ($(controlButton).hasClass("down"))
+        {
+            $(controlButton).parent().find(".container").slideDown();
+            $(controlButton).animate({marginTop : "-19px"}, "fast");
+        }
+        else
+        {
+            $(controlButton).parent().find(".container").slideUp();
+            $(controlButton).animate({marginTop : "0px"}, "fast");
+        }
+        $(controlButton).toggleClass("down");
+}
+
+$(document).ready(function(){
+    addNotification("Kyky1");
+    addNotification("Kyky2");
+    
+    $("HEADER .control").live("click", function(){
         toggleFriendsBar(this);
     });
 });
