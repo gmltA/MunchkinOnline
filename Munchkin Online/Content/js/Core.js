1d21627eb67c8037d006134333f6f89b940c78bc
gmlt.A <gmltt@yandex.ru>
2014-09-13 09:18:34 +0300

    Move norification-related functions to class

--- Munchkin Online/Content/js/Core.js
+++ Munchkin Online/Content/js/Core.js
@@ -1,66 +1,66 @@
-﻿var notificationQueue = [];
-var isNotificationActive = false;
+﻿function NotificationMgr() {
+    this.notificationQueue = [];
+    this.isNotificationActive = false;
+}
 
-function showNotification(message)
-{
-    isNotificationActive = true;
+NotificationMgr.prototype.addNotification = function (message) {
+    this.notificationQueue.push(message);
+    if (this.isNotificationActive == false)
+        this.displayNextNotification();
+}
+
+NotificationMgr.prototype.showNotification = function (message) {
+    this.isNotificationActive = true;
     $(".notification").addClass("anim");
-    $(".notification").animate({bottom : '0'}, 250, "swing", function(){
-        $(".notification > .center").animate({width : '190px'}, 500, "swing", function(){
+    $(".notification").animate({ bottom: '0' }, 250, "swing", function () {
+        $(".notification > .center").animate({ width: '190px' }, 500, "swing", function () {
             $(".notification > .scroll").hide();
             $(".notification").toggleClass("anim");
             $(".notification > P").text(message);
         })
     });
-    setTimeout(function()
-    {
+    var mgr = this;
+    setTimeout(function () {
         $(".notification").toggleClass("anim");
         $(".notification > P").text("");
         $(".notification > .scroll").show();
-        $(".notification > .center").animate({width : '0px'}, 500, "swing", function(){
-            $(".notification").animate({bottom : '-200px'}, 250, "swing", function(){
-                isNotificationActive = false;
-                displayNextNotification();
+        $(".notification > .center").animate({ width: '0px' }, 500, "swing", function () {
+            $(".notification").animate({ bottom: '-200px' }, 250, "swing", function () {
+                mgr.isNotificationActive = false;
+                mgr.displayNextNotification();
             })
         });
-	}, 1000);
+    }, 1000);
 }
 
-function displayNextNotification()
-{
-    if (notificationQueue.length == 0)
+NotificationMgr.prototype.displayNextNotification = function () {
+    if (this.notificationQueue.length == 0)
         return;
 
-    showNotification(notificationQueue.pop());
+    this.showNotification(this.notificationQueue.pop());
 }
 
-function addNotification(message)
-{
-    notificationQueue.push(message);
-    if (isNotificationActive == false)
-        displayNextNotification();
+var notificationMgr = new NotificationMgr();
+
+function toggleFriendsBar(controlButton) {
+    if ($(controlButton).hasClass("down")) {
+        $(controlButton).parent().find(".container").slideDown();
+        $(controlButton).animate({ marginTop: "-19px" }, "fast");
+    }
+    else {
+        $(controlButton).parent().find(".container").slideUp();
+        $(controlButton).animate({ marginTop: "0px" }, "fast");
+    }
+    $(controlButton).toggleClass("down");
 }
 
-function toggleFriendsBar(controlButton)
-{
-    if ($(controlButton).hasClass("down"))
-        {
-            $(controlButton).parent().find(".container").slideDown();
-            $(controlButton).animate({marginTop : "-19px"}, "fast");
-        }
-        else
-        {
-            $(controlButton).parent().find(".container").slideUp();
-            $(controlButton).animate({marginTop : "0px"}, "fast");
-        }
-        $(controlButton).toggleClass("down");
-}
+$(document).ready(function () {
+    setTimeout(function () {
+        notificationMgr.addNotification("Kyky1");
+        notificationMgr.addNotification("Kyky2");
+    }, 1000);
 
-$(document).ready(function(){
-    addNotification("Kyky1");
-    addNotification("Kyky2");
-    
-    $("HEADER .control").live("click", function(){
+    $("HEADER .control").live("click", function () {
         toggleFriendsBar(this);
     });
 });
