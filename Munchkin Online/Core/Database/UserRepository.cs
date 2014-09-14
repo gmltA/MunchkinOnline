017e5a533daff977748b468bb41b0e95448226d3
zaq007 <sergey4000@tut.by>
2014-09-14 17:01:22 +0300

    EF 6.1. Added User's db interface

---
+++ Munchkin Online/Core/Database/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Munchkin_Online.Models;
+
+namespace Munchkin_Online.Core.Database
+{
+    public class UserRepository
+    {
+        MainContext DB = new MainContext();
+
+        public IQueryable<User> Accounts
+        {
+            get
+            {
+                return DB.Users;
+            }
+        }
+
+        public bool Add(User instance)
+        {
+            try
+            {
+                DB.Users.Add(instance);
+                DB.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+}
