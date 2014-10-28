2fd8de178124328fbdfefd45cb4723f3148a59e0
gmlt.A <gmltt@yandex.ru>
2014-10-28 19:05:50 +0300

    Rewrite LongPool.js (Extract prototype)
    ToDo: cleanup

--- Munchkin Online/Content/js/LongPool.js
+++ Munchkin Online/Content/js/LongPool.js
@@ -1,70 +1,101 @@
-﻿$(document).ready(function () {
+﻿function LongPool()
+{
+}
+
+LongPool.prototype = {
+    connect: function ()
+    {
+        var url = '/LongPoolHandler.ashx?cmd=' + currAction;
+        var self = this;
+        $.ajax({
+            type: "POST",
+            url: url,
+            success: function (response)
+            {
+                self.processResponse(response)
+            },
+            error: this.connectionRefused
+        });
+    },
+    disconnect: function ()
+    {
+        var url = '/LongPoolHandler.ashx?cmd=unregister';
+        $.ajax({
+            type: "POST",
+            url: url
+        });
+    },
+    sendRequest: function ()
+    {
+        var url = '/LongPoolHandler.ashx';
+        var self = this;
+        $.ajax({
+            type: "POST",
+            url: url,
+            success: function (response)
+            {
+                self.processResponse(response)
+            },
+            error: this.sendRequest
+        });
+    },
+    processFindGameResult: function (message)
+    {
+        if (notificationMgr.isStickyInfoActive)
+            notificationMgr.updateStickyText(message);
+        else
+            notificationMgr.addNotification(message, 0, true);
+    },
+    processResponse: function (transport)
+    {
+        transport = JSON.parse(transport);
+        switch (transport.Type) {
+            case 3: $(".find").html("<a onclick=\"accept()\">Принять</a>"); break;
+            case 5: notificationMgr.addNotification(transport.Data); break;
+            case 6: break;
+            case 7: this.processFindGameResult(transport.Data); break;
+            case 8: return;
+        }
+        this.sendRequest();
+    },
+    accept: function (transport)
+    {
+        var url = '/LongPoolHandler.ashx?cmd=MatchConfirmation';
+        var self = this;
+        $.ajax({
+            type: "POST",
+            success: function (response)
+            {
+                self.processResponse(response)
+            },
+            error: this.sendRequest,
+            url: url
+        });
+    },
+    connectionRefused: function ()
+    {
+        //$("body").html("не удалось подключиться к серверу.Попробуем через 3 секунды...");
+        //setTimeout(Connect(), 3000);
+    }
+}
+
+var longPool = new LongPool();
+
+function findGame()
+{
+    currAction = 'FindMatch';
+    longPool.connect();
+}
+
+$(document).ready(function ()
+{
+    //findGame();
+    //TODO: WTF is this shit?
     if (typeof currAction === 'undefined') {
         currAction = '';
     }
-    Connect();
 });
 
 $(window).unload(function () {
     //Disconnect();
 });
-
-function SendRequest() {
-    var url = '/LongPoolHandler.ashx';
-    $.ajax({
-        type: "POST",
-        url: url,
-        success: ProcessResponse,
-        error: SendRequest
-    });
-}
-
-function Connect() {
-    var url = '/LongPoolHandler.ashx?cmd=' + currAction;
-    $.ajax({
-        type: "POST",
-        url: url,
-        success: ProcessResponse,
-        error: ConnectionRefused
-    });
-}
-
-function Disconnect() {
-    var url = '/LongPoolHandler.ashx?cmd=unregister';
-    $.ajax({
-        type: "POST",
-        url: url
-    });
-}
-
-function ProcessResponse(transport) {
-    transport = JSON.parse(transport);
-    switch (transport.Type) {
-        case 3: $(".find").html("<a onclick=\"accept()\">Принять</a>"); break; //Матч найден, требуется подтверждение
-        case 5: notificationMgr.addNotification(transport.Data); break;
-        case 6: break; //Не все подтвердили
-        case 7: $("#QueueStatus").html(transport.Data); break;
-        case 8: return; //need stop pooling
-    }
-    SendRequest();
-}
-
-function accept() {
-    var url = '/LongPoolHandler.ashx?cmd=MatchConfirmation';
-    $.ajax({
-        type: "POST",
-        success: ProcessResponse,
-        error: SendRequest,
-        url: url
-    });
-}
-
-function OnConnected(transport) {
-    //clientGuid = transport;
-    SendRequest();
-}
-
-function ConnectionRefused() {
-    //$("body").html("не удалось подключиться к серверу.Попробуем через 3 секунды...");
-    //setTimeout(Connect(), 3000);
-}
