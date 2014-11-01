16c7f26b1661a60792630f4a7ab191887709d1fb
zaq007 <sergey4000@tut.by>
2014-11-01 12:34:39 +0300

    Import site moto from node.js instance

---
+++ Munchkin Online/Core/MotoImporter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace Munchkin_Online.Core
+{
+    public static class MotoImporter
+    {
+        static Uri NodePath;
+
+        static MotoImporter()
+        {
+            UriBuilder ub = new UriBuilder();
+            ub.Scheme = "http";
+            ub.Host = "localhost";
+            ub.Port = 8888;
+            NodePath = ub.Uri;
+        }
+
+        public static string GetMoto()
+        {
+            try
+            {
+                return new StreamReader(((HttpWebRequest)WebRequest.Create(NodePath)).GetResponse().GetResponseStream()).ReadToEnd();
+            }
+            catch
+            {
+                return "Убивай гусей,<br/>подставляй друзей!";
+            }
+        }
+    }
+}
