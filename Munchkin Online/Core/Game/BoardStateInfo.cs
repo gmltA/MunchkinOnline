943eaad2a03aac805ca5e7c6e27509e670de6362
zaq007 <sergey4000@tut.by>
2014-11-16 00:32:14 +0300

    Missed files

---
+++ Munchkin Online/Core/Game/BoardStateInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Munchkin_Online.Core.Auth;
+
+namespace Munchkin_Online.Core.Game
+{
+    public class BoardStateInfo
+    {
+        public const int PLAYERS_COUNT = 4;
+
+        public int CurrentPlayerId { get; set; }
+        public List<PlayerInfo> Players { get; set; }
+        public Player Me { get; set; }
+
+        public TurnStep TurnStep { get; set; }
+        public Battle Battle { get; set; }
+
+        public BoardStateInfo(BoardState b)
+        {
+            CurrentPlayerId = b.CurrentPlayerId;
+            Players = new List<PlayerInfo>();
+            foreach (var p in b.Players)
+            {
+                if (p.UserId == CurrentUser.Instance.Current.Id)
+                    Me = p;
+                else
+                    Players.Add(new PlayerInfo(p));
+            }
+            TurnStep = b.TurnStep;
+            Battle = b.Battle;
+        }
+    }
+}
---
+++ Munchkin Online/Core/Game/PlayerInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Munchkin_Online.Models;
+
+namespace Munchkin_Online.Core.Game
+{
+    public class PlayerInfo
+    {
+        public Guid UserId { get; set; }
+
+        public int Level { get; set; }
+
+        public int TreasuresCount { get; set; }
+        public int DoorsCount { get; set; }
+        public List<Card> Board { get; set; }
+
+        public PlayerInfo(Player p)
+        {
+            UserId = p.UserId;
+            Level = p.Level;
+            Board = p.Board;
+            DoorsCount = p.Hand.Where(x=>x.Type == CardType.Dungeon).Count();
+            TreasuresCount = p.Hand.Where(x => x.Type == CardType.Treasure).Count();
+        }
+    }
+}
